7d504b411c0f4788af01736141fb71a15fcd1413
d3adb5 <me@d3adb5.net>
2022-04-12 18:12:17 -0300

    feat(xmonad): add modifier to hide windows automatically
    Add a modifier to hide the Nth window once the workspace reaches a
    specific number of windows. This is a bit of hack, as ideally this would
    behave just like "limit" and be able to take a single parameter, with
    the window to be hidden being the one focused immediately before a new
    window was added to the stack.
    
    This is pending some refactoring, but for now it works.

--- xmonad/lib/XMonad/Layout/Hidden.hs
+++ xmonad/lib/XMonad/Layout/Hidden.hs
@@ -5,21 +5,29 @@
 
 module XMonad.Layout.Hidden
   ( hidden
+  , hideNAt
   ) where
 
 import XMonad
 import qualified XMonad.StackSet as W
-import XMonad.Util.Hidden (Hidden(..))
+import XMonad.Actions.Hidden
+import XMonad.Util.Hidden
 import XMonad.Util.ExtensibleState as XS
 import XMonad.Layout.LayoutModifier
 import qualified XMonad.Layout.BoringWindows as BW
 
+import Control.Monad (when)
 import Data.Foldable (toList)
+import Data.List (splitAt)
+import Data.Sequence ((<|))
 
-data HiddenL a = HiddenL deriving (Read, Show)
+data HiddenL a = HiddenL         deriving (Read, Show)
+data HideNAt a = HideNAt Int Int deriving (Read, Show)
 
 hidden :: l Window -> ModifiedLayout HiddenL l Window
 hidden = ModifiedLayout HiddenL
+
+hideNAt n at = hidden . ModifiedLayout (HideNAt n at)
 
 instance LayoutModifier HiddenL Window where
   modifierDescription = const "Hidden"
@@ -36,3 +44,32 @@ instance LayoutModifier HiddenL Window where
         flip sendMessageWithNoRefresh ws $ BW.Replace "Hidden" (toList hidden)
         return Nothing
     | otherwise = return Nothing
+
+instance LayoutModifier HideNAt Window where
+  modifierDescription (HideNAt n at) = "Hide " ++ show n ++ " at " ++ show at
+
+  modifyLayout (HideNAt n at) wksp rect = do
+    let visibleWindows = W.integrate' (W.stack wksp)
+    hiddenWindows <- getHidden
+
+    let willHideWindow = length visibleWindows > at
+        willUnhideWindow = length hiddenWindows > 1 && length visibleWindows < at
+        stackToBeTiled | willHideWindow = deleteNthElem n (W.stack wksp)
+                       | willUnhideWindow = prepend (rightmost hiddenWindows) (W.stack wksp)
+                       | otherwise = W.stack wksp
+
+    when willUnhideWindow $ unhideWindowAndAct (rightmost hiddenWindows) (pure ())
+    when willHideWindow $ hideWindowAndAct (<|) (visibleWindows !! (n - 1)) (pure ())
+    runLayout (wksp { W.stack = stackToBeTiled }) rect
+
+deleteNthElem :: Int -> Maybe (W.Stack a) -> Maybe (W.Stack a)
+deleteNthElem n s =
+  let listFromStack = W.integrate' s
+      (start, end) = splitAt n listFromStack
+      nRemoved = take (n - 1) start ++ end
+  in W.differentiate nRemoved
+
+prepend :: a -> Maybe (W.Stack a) -> Maybe (W.Stack a)
+prepend w s =
+  let listFromStack = W.integrate' s
+  in W.differentiate (w : listFromStack)
