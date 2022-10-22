33143f114af618a54f54b155bf85714b723aacb9
d3adb5 <me@d3adb5.net>
2022-10-22 16:09:34 -0300

    chore(nvim): set up lsp keybinds only on load
    Set up the LSP plugin keybinds only when a language server is loaded up.
    Some other updates are built into this commit, such as capabilities that
    were coming from a deprecated function and the diagnostics functions
    that are not situated elsewhere.

--- neovim/init.lua
+++ neovim/init.lua
@@ -122,23 +122,6 @@ require("packer").startup(function ()
     config = function () require("completion") end
   }
 
-  use { "neovim/nvim-lspconfig",
-    config = function ()
-      vim.keymap.set("n", "K",     vim.lsp.buf.hover)
-      vim.keymap.set("n", "gd",    vim.lsp.buf.definition)
-      vim.keymap.set("n", "gi",    vim.lsp.buf.implementation)
-      vim.keymap.set("n", "gr",    vim.lsp.buf.references)
-      vim.keymap.set("n", "gD",    vim.lsp.buf.declaration)
-      vim.keymap.set("n", "<C-s>", vim.lsp.buf.signature_help)
-
-      vim.keymap.set("n", "<leader>f",  vim.lsp.buf.formatting)
-      vim.keymap.set("n", "<leader>D",  vim.lsp.buf.type_definition)
-      vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename)
-      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action)
-
-      vim.keymap.set("n", "<C-k>", vim.lsp.diagnostic.goto_prev)
-      vim.keymap.set("n", "<C-j>", vim.lsp.diagnostic.goto_next)
-    end
-  }
+  use "neovim/nvim-lspconfig"
   -- }}}
 end)
--- neovim/lua/completion.lua
+++ neovim/lua/completion.lua
@@ -1,7 +1,21 @@
 local cmp = require("cmp")
 local lsp = require("lspconfig")
-local cap = require("cmp_nvim_lsp")
-  .update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local cap = require("cmp_nvim_lsp").default_capabilities()
+
+local on_attach = function (client, bufnr)
+  local bufopts = { noremap = true, silent = true, buffer = bufnr }
+  vim.keymap.set("n", "K",     vim.lsp.buf.hover, bufopts)
+  vim.keymap.set("n", "gd",    vim.lsp.buf.definition, bufopts)
+  vim.keymap.set("n", "gi",    vim.lsp.buf.implementation, bufopts)
+  vim.keymap.set("n", "gr",    vim.lsp.buf.references, bufopts)
+  vim.keymap.set("n", "gD",    vim.lsp.buf.declaration, bufopts)
+  vim.keymap.set("n", "<C-s>", vim.lsp.buf.signature_help, bufopts)
+
+  vim.keymap.set("n", "<leader>f",  vim.lsp.buf.formatting, bufopts)
+  vim.keymap.set("n", "<leader>D",  vim.lsp.buf.type_definition, bufopts)
+  vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, bufopts)
+  vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, bufopts)
+end
 
 cmp.setup({
   snippet = { expand = function (a) vim.fn["UltiSnips#Anon"](a.body) end },
@@ -12,8 +26,8 @@ cmp.setup({
   }
 })
 
-lsp.groovyls.setup({ capabilities = cap })
-lsp.pyright.setup({ capabilities = cap })
-lsp.terraformls.setup({ capabilities = cap })
-lsp.hls.setup({ capabilities = cap })
-lsp.sumneko_lua.setup({ capabilities = cap })
+lsp.groovyls.setup({ on_attach = on_attach, capabilities = cap })
+lsp.pyright.setup({ on_attach = on_attach, capabilities = cap })
+lsp.terraformls.setup({ on_attach = on_attach, capabilities = cap })
+lsp.hls.setup({ on_attach = on_attach, capabilities = cap })
+lsp.sumneko_lua.setup({ on_attach = on_attach, capabilities = cap })
