eac76246de7ffc4bb499e3f9b6a3b9f47cdb7aa4
d3adb5 <me@d3adb5.net>
2023-06-01 21:52:17 -0700

    refactor(xmonad): remove unused language pragmas
    Remove unused GHC language pragmas from the XMonad configuration.

--- xmonad/lib/XMonad/Layout/HiddenQueueLayout.hs
+++ xmonad/lib/XMonad/Layout/HiddenQueueLayout.hs
@@ -1,7 +1,5 @@
-{-# LANGUAGE DeriveDataTypeable #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE TupleSections #-}
 
