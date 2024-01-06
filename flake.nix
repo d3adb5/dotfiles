eb2bd35ed87d513072d439a49e410abddc3d06b3
d3adb5 <me@d3adb5.net>
2024-01-05 20:55:48 -0800

    chore(nix): update flake inputs in flake.lock
    Update flake inputs in flake.lock.

--- flake.nix
+++ flake.nix
@@ -2,8 +2,8 @@
   description = "d3adb5's NixOS configuration";
 
   inputs = {
-    nixpkgs.url = github:nixos/nixpkgs/nixos-23.05;
-    home-manager.url = github:nix-community/home-manager/release-23.05;
+    nixpkgs.url = github:nixos/nixpkgs/nixos-23.11;
+    home-manager.url = github:nix-community/home-manager/release-23.11;
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     nur.url = github:nix-community/nur;
   };
